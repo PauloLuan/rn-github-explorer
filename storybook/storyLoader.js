b0bb2926e322aa5aaa0abcbfb05d47fd83935976
Paulo Luan <pauloluan.inova@gmail.com>
2022-02-24 19:09:00 -0300

    ✨ feat: added storybook config files

---
+++ storybook/storyLoader.js
@@ -0,0 +1,19 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require('../src/components/button/button.stories')
+  require('../src/screens/sign-in/SignIn.stories')
+}
+
+const stories = [
+  '../src/components/button/button.stories',
+  '../src/screens/sign-in/SignIn.stories'
+]
+
+module.exports = {
+  loadStories,
+  stories
+}

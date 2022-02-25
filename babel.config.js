a8ee8275bb8b3f18c3865072b0d44a0e3bfc5048
Paulo Luan <pauloluan.inova@gmail.com>
2022-02-25 08:56:32 -0300

    👌 improve: mapped redux paths

--- babel.config.js
+++ babel.config.js
@@ -16,6 +16,7 @@ module.exports = function (api) {
             '@hooks': ['./src/hooks'],
             '@navigation': ['./src/navigation'],
             '@screens': ['./src/screens'],
+            '@redux': ['./src/redux'],
             '@types': ['./src/types']
           }
         }

5c641e5e319a1fed15e73c94f67c315dd883c60a
Paulo Luan <pauloluan.inova@gmail.com>
2022-02-25 08:30:05 -0300

    ✅ test: added utils for react navigation mock

--- src/utils/test-helper.tsx
+++ src/utils/test-helper.tsx
@@ -5,3 +5,19 @@ import theme from '../global/styles/theme'
 
 export const renderWithTheme = (children: React.ReactNode): RenderAPI =>
   render(<ThemeProvider theme={theme}>{children}</ThemeProvider>)
+
+export const mockNavigation = () => {
+  const mockedNavigate = jest.fn()
+
+  jest.mock('@react-navigation/native', () => {
+    const actualNav = jest.requireActual('@react-navigation/native')
+    return {
+      ...actualNav,
+      useNavigation: () => ({
+        navigate: mockedNavigate
+      })
+    }
+  })
+
+  return { mockedNavigate }
+}

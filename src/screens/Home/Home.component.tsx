553be58cd6a1ee59c3874b8dec470d7fb77ab12a
Paulo Luan <pauloluan.inova@gmail.com>
2022-02-25 08:57:19 -0300

    📦 new: added simple version of dispatch use on home screen

--- src/screens/Home/Home.component.tsx
+++ src/screens/Home/Home.component.tsx
@@ -2,18 +2,24 @@ import { Button } from '@components/button/button.component'
 import React from 'react'
 import { Button as RNButton, StatusBar } from 'react-native'
 import * as S from './Home.styles'
-import { useNavigation } from '@react-navigation/native'
-import { RootStackParamList } from '@navigation/RootStackParams'
-import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+// import { useNavigation } from '@react-navigation/native'
+// import { RootStackParamList } from '@navigation/RootStackParams'
+// import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 
-type homeScreenProp = NativeStackNavigationProp<RootStackParamList, 'Home'>
+import { useAppSelector, useAppDispatch } from '@redux/hooks'
+import { increment } from '@redux/repositoriesSlice'
+
+// type homeScreenProp = NativeStackNavigationProp<RootStackParamList, 'Home'>
 
 export interface HomeProps {
   testId?: string
 }
 
 export const Home = ({ testId = 'component-home-id', ...props }: HomeProps) => {
-  const navigation = useNavigation<homeScreenProp>()
+  // const navigation = useNavigation<homeScreenProp>()
+
+  const count = useAppSelector((state) => state.repositories.value)
+  const dispatch = useAppDispatch()
 
   return (
     <S.Wrapper testID={testId} {...props}>
@@ -32,10 +38,11 @@ export const Home = ({ testId = 'component-home-id', ...props }: HomeProps) => {
           stack
         </S.SubHeading>
         <Button />
+
         <RNButton
           title="teste"
           onPress={() => {
-            navigation.navigate('List')
+            dispatch(increment())
           }}
         />
       </S.Container>
